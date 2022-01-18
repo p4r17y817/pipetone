d0a21ec906dd61ba94e94bfc9b342c53da83e855
wabisabia <bkkchris.dunn@outlook.com>
2022-01-18 21:08:18 +0000

    Performance and documentation improvements

--- src/main.rs
+++ src/main.rs
@@ -1,189 +1,80 @@
+use clap::Parser;
 use csv::Writer;
 use image::{
     imageops::crop, imageops::grayscale, imageops::invert, imageops::resize, imageops::FilterType,
-    open, DynamicImage, GenericImageView, GrayImage, Luma, SubImage,
+    DynamicImage, GenericImageView, GrayImage, Luma, SubImage,
 };
 use nalgebra::{vector, EuclideanNorm, Norm};
 use ndarray::{Array, Array1};
 use rayon::prelude::*;
-use std::{cmp, f32::consts::PI, path::PathBuf};
-use structopt::StructOpt;
+use std::{cmp, f32::consts::PI, ops::RangeInclusive, path::PathBuf};
 
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "pipetone 🧵",
-    about = "🚀 A fast, Rusty port of threadTone.py (https://github.com/theveloped/ThreadTone)
-Generates a circular, halftone representation of an image using \"threads\"."
-)]
+#[derive(Debug, Clone, Parser)]
+#[clap(author, version, about, long_about = None)]
 struct Opt {
-    #[structopt(parse(from_os_str), help = "Path to target image")]
+    #[clap(parse(from_os_str))]
+    /// Path to the target image
     path: PathBuf,
-    #[structopt(short, long, default_value = "500", help = "# pins on loom")]
+    #[clap(short, long, default_value = "500")]
+    /// Number of pins on the loom
     pins: usize,
-    #[structopt(short, long, default_value = "1000", help = "MAX # threads")]
+    #[clap(short, long, default_value = "1000")]
+    /// Maximum number of threads used
     threads: usize,
-    #[structopt(
-        short,
-        long,
-        help = "Radius of output image in pixels. Should be no greater than l = min(input_width, input_height). Defaults to l otherwise, or if omitted"
-    )]
+    #[clap(short, long)]
+    /// Desired radius of the output image in pixels. Should be no greater than l = min(input_width, input_height). Defaults to l otherwise, or if omitted
     radius: Option<u32>,
-    #[structopt(
-        short,
-        long,
-        parse(from_os_str),
-        help = "Path to output image. Defaults to input's directory"
-    )]
+    #[clap(short, long, parse(from_os_str))]
+    /// Path to output image. Defaults to the parent directory of the input image
     output: Option<PathBuf>,
-    #[structopt(
-        long,
-        help = "Save thread start and end coordinates as CSV. Unset by default"
-    )]
+    #[clap(long)]
+    /// Save thread information to a CSV
+    #[clap(group = "use_csv")]
     csv: bool,
-    #[structopt(
-        long,
-        help = "Include CSV header line. If --write-coords then the value is \"x1, y1, x2, y2\", otherwise \"pins\". Unset by default. Used with --csv"
-    )]
-    header: bool,
-    #[structopt(
-        long,
-        help = "Skip image generation. Unset by default. Used with --csv"
-    )]
+    #[clap(long, requires = "use_csv")]
+    /// Skip image generation
     no_img: bool,
-    #[structopt(
-        long,
-        help = "Write thread end-point pixel coordinates instead of pin numbers to CSV. Used with --csv"
-    )]
+    #[clap(long, requires = "use_csv")]
+    /// Write the pixel-coordinates of each ends of a thread to the CSV instead of pin numbers
     write_coords: bool,
+    #[clap(long, requires = "use_csv")]
+    /// Include a CSV header line. If --write-coords then the header line is `x1, y1, x2, y2`, otherwise `pins`
+    header: bool,
 }
 
 fn main() {
-    println!(
-        "           _            __                 
-    ____  (_)___  ___  / /_____  ____  ___ 
-   / __ \\/ / __ \\/ _ \\/ __/ __ \\/ __ \\/ _ \\
-  / /_/ / / /_/ /  __/ /_/ /_/ / / / /  __/
- / .___/_/ .___/\\___/\\__/\\____/_/ /_/\\___/ 
-/_/     /_/
-"
-    );
+    let opt = Opt::parse();
 
-    let Opt {
-        path,
-        pins,
-        threads,
-        radius,
-        output,
-        csv,
-        header,
-        no_img,
-        write_coords,
-    } = Opt::from_args();
+    let img = image::open(&opt.path).expect("Couldn't load target image");
 
-    let img = open(&path).expect("Couldn't load target image");
-    let min_edge_length = img.width().min(img.height());
-    let radius = radius.map_or(min_edge_length, |radius| radius.min(min_edge_length));
+    let min_edge = img.width().min(img.height());
+    let radius = opt.radius.map_or(min_edge, |radius| radius.min(min_edge));
     let length = radius * 2 + 1;
 
-    let thread_coords = generate_threads(img, threads, pins, radius, length);
+    let preprocessed = preprocess(img, radius, length);
+
+    let thread_coords = thread(preprocessed, radius, &opt);
 
     let prefix = format!(
         "{}_{}_{}",
-        path.file_stem().unwrap().to_str().unwrap(),
-        pins,
-        threads
+        opt.path.file_stem().unwrap().to_str().unwrap(),
+        opt.pins,
+        opt.threads
     );
-    let mut out_dir = output.unwrap_or(path);
+    let mut outfile = opt.clone().output.unwrap_or(opt.clone().path);
 
-    if !no_img {
-        save_img(&mut out_dir, &prefix, &thread_coords, length);
+    if !opt.no_img {
+        write_img(&mut outfile, &prefix, &thread_coords, length);
     }
 
-    if csv {
-        let optional_header = header.then(|| {
-            if write_coords {
-                vec!["x1", "y1", "x2", "y2"]
-            } else {
-                vec!["pins"]
-            }
-        });
-        save_csv(
-            &mut out_dir,
-            &prefix,
-            &thread_coords,
-            optional_header,
-            write_coords,
-        );
+    if opt.csv {
+        write_csv(&mut outfile, &prefix, &thread_coords, &opt);
     }
 }
 
-fn save_img(
-    out_dir: &mut PathBuf,
-    prefix: &str,
-    thread_coords: &[(Array1<f64>, Array1<f64>, usize)],
-    length: u32,
-) {
-    let mut img_threaded = GrayImage::from_pixel(length, length, Luma([255]));
-    for (x_line, y_line, _) in thread_coords {
-        #[allow(clippy::cast_sign_loss)] // coordinates are positive
-        #[allow(clippy::cast_possible_truncation)] // truncation is desired
-        x_line.iter().zip(y_line.iter()).for_each(|(x, y)| {
-            img_threaded[(*x as u32, *y as u32)].0[0] = 0;
-        });
-    }
-    out_dir.set_file_name(format!("{}_threaded", prefix));
-    out_dir.set_extension("png");
-    img_threaded
-        .save(&out_dir)
-        .expect("Failed to save threaded image");
-}
-
-fn save_csv(
-    out_dir: &mut PathBuf,
-    prefix: &str,
-    thread_coords: &[(Array1<f64>, Array1<f64>, usize)],
-    optional_header: Option<Vec<&str>>,
-    write_threads: bool,
-) {
-    out_dir.set_file_name(format!("{}_threads", prefix));
-    out_dir.set_extension("csv");
-    let mut writer = Writer::from_path(&out_dir).expect("Failed to save threads CSV");
-    if let Some(header) = optional_header {
-        writer
-            .write_record(&header)
-            .expect("Failed to write header");
-    }
-    let formatter = if write_threads {
-        |(x_line, y_line, _): &(Array1<f64>, Array1<f64>, usize)| {
-            let last = x_line.len() - 1;
-            vec![
-                format!("{}", x_line[0] as u32),
-                format!("{}", y_line[0] as u32),
-                format!("{}", x_line[last] as u32),
-                format!("{}", y_line[last] as u32),
-            ]
-        }
-    } else {
-        |(_, _, pin): &(Array1<f64>, Array1<f64>, usize)| vec![format!("{}", pin)]
-    };
-    for thread_coord in thread_coords {
-        writer
-            .write_record(formatter(thread_coord))
-            .expect("Failed to write thread");
-    }
-}
-
-fn generate_threads(
-    img: DynamicImage,
-    max_threads: usize,
-    num_pins: usize,
-    radius: u32,
-    length: u32,
-) -> Vec<(Array1<f64>, Array1<f64>, usize)> {
-    let mut img_preprocessed = preprocess_img(img, radius, length);
-
-    let pin_positions = Array::linspace(0., 2. * PI, num_pins + 1)
-        .to_vec()
+fn thread(mut img: GrayImage, radius: u32, opt: &Opt) -> Vec<(Array1<f64>, Array1<f64>, usize)> {
+    let num_pins = opt.pins + 1;
+    let loom = Array::linspace(0., 2. * PI, num_pins)
         .into_iter()
         .map(|alpha| {
             vector![
@@ -193,16 +84,22 @@ fn generate_threads(
         })
         .collect::<Vec<_>>();
 
-    let mut threads = Vec::with_capacity(max_threads);
+    let mut threads = Vec::with_capacity(opt.threads);
     let mut prev_pins = [0; 2];
 
-    for i in 0..max_threads {
+    for _ in 0..opt.threads {
         let prev_pin = prev_pins[1];
-        let prev_pos = &pin_positions[prev_pin];
-        let lines = pin_positions[prev_pin..]
+        let prev_pos = &loom[prev_pin];
+
+        let best = loom[prev_pin..]
             .par_iter()
-            .chain(&pin_positions[..prev_pin])
-            .map(|next_pos| {
+            .chain(&loom[..prev_pin])
+            .enumerate()
+            .filter_map(|(i, next_pos)| {
+                let next_pin = (prev_pin + i) % num_pins;
+                (!prev_pins.contains(&next_pin)).then(|| (next_pin, next_pos))
+            })
+            .map(|(current_pin, next_pos)| {
                 #[allow(clippy::cast_sign_loss)] // distance is positive
                 #[allow(clippy::cast_possible_truncation)]
                 // truncation is desired
@@ -216,18 +113,14 @@ fn generate_threads(
                     .iter()
                     .zip(y_line.iter())
                     .map(|(&x, &y)| {
-                        u32::from(img_preprocessed[(x.floor() as u32, y.floor() as u32)][0])
+                        let pixel_idx = pos_to_pixel_idx(x, y, &img);
+                        u32::from(
+                            // XXX: To change to `get_pixel_unchecked` once image v0.24 lands
+                            unsafe { img.get_unchecked(pixel_idx) }[0],
+                        )
                     })
                     .sum();
-                (x_line, y_line, line_sum)
-            });
-        let best = (1..num_pins)
-            .into_par_iter()
-            .map(|j| (prev_pin + j) % num_pins)
-            .zip(lines)
-            .filter(|(next_pin, _)| !prev_pins.contains(&next_pin))
-            .map(|(next_pin, (x_line, y_line, line_sum))| {
-                Line::new(next_pin, x_line, y_line, line_sum)
+                Line::new(current_pin, x_line, y_line, line_sum)
             })
             .max()
             .unwrap();
@@ -237,20 +130,25 @@ fn generate_threads(
         threads.push((best.xs.clone(), best.ys.clone(), best.dest_pin));
         #[allow(clippy::cast_sign_loss)] // coordinates are positive
         #[allow(clippy::cast_possible_truncation)] // truncation is desired
-        best.xs.iter().zip(best.ys.iter()).for_each(|(&x, &y)| {
-            img_preprocessed[(x as u32, y as u32)][0] = 0;
+        best.xs.into_iter().zip(best.ys).for_each(|(x, y)| {
+            let pixel_idx = pos_to_pixel_idx(x, y, &img);
+            let pixel = unsafe { img.get_unchecked_mut(pixel_idx) };
+            pixel[0] = 0
         });
 
         if best.dest_pin == prev_pin {
             break;
         }
-
-        print!("\rPlacing thread {}/{}", i + 1, max_threads);
     }
-    println!();
     threads
 }
 
+fn pos_to_pixel_idx<I: GenericImageView>(x: f64, y: f64, img: &I) -> RangeInclusive<usize> {
+    let min_idx = y.floor() as usize * img.width() as usize + x.floor() as usize;
+    min_idx..=min_idx
+}
+
+#[derive(Clone)]
 struct Line {
     dest_pin: usize,
     xs: Array1<f64>,
@@ -289,21 +187,19 @@ impl PartialOrd<Line> for Line {
     }
 }
 
-fn preprocess_img(mut img: DynamicImage, radius: u32, length: u32) -> GrayImage {
-    let img_cropped = square_crop(&mut img);
-    let img_gray = grayscale(&img_cropped);
-    let mut img_resized = resize(&img_gray, length, length, FilterType::Nearest);
-    invert(&mut img_resized);
-    img_resized
-        .enumerate_pixels_mut()
-        .for_each(|(x, y, pixel)| {
-            if (x.saturating_sub(length / 2)).pow(2) + (y.saturating_sub(length / 2)).pow(2)
-                > radius.pow(2)
-            {
-                pixel[0] = 0;
-            }
-        });
-    img_resized
+fn preprocess(mut img: DynamicImage, radius: u32, length: u32) -> GrayImage {
+    let cropped = square_crop(&mut img);
+    let gray = grayscale(&cropped);
+    let mut resized = resize(&gray, length, length, FilterType::Nearest);
+    invert(&mut resized);
+    resized.enumerate_pixels_mut().for_each(|(x, y, pixel)| {
+        if (x.saturating_sub(length / 2)).pow(2) + (y.saturating_sub(length / 2)).pow(2)
+            > radius.pow(2)
+        {
+            pixel[0] = 0;
+        }
+    });
+    resized
 }
 
 fn square_crop<I: GenericImageView>(img: &mut I) -> SubImage<&mut I> {
@@ -312,4 +208,82 @@ fn square_crop<I: GenericImageView>(img: &mut I) -> SubImage<&mut I> {
     let top_edge = (height - min_edge) / 2;
     let left_edge = (width - min_edge) / 2;
     crop(img, left_edge, top_edge, min_edge, min_edge)
+}
+
+fn write_img(
+    outfile: &mut PathBuf,
+    prefix: &str,
+    thread_coords: &[(Array1<f64>, Array1<f64>, usize)],
+    length: u32,
+) {
+    let mut img_threaded = GrayImage::from_pixel(length, length, Luma([255]));
+    for (x_line, y_line, _) in thread_coords {
+        #[allow(clippy::cast_sign_loss)] // coordinates are positive
+        #[allow(clippy::cast_possible_truncation)] // truncation is desired
+        x_line.into_iter().zip(y_line).for_each(|(&x, &y)| {
+            let pixel_idx = pos_to_pixel_idx(x, y, &img_threaded);
+            let pixel = unsafe { img_threaded.get_unchecked_mut(pixel_idx) };
+            pixel[0] = 0;
+        });
+    }
+    outfile.set_file_name(format!("{}_threaded", prefix));
+    outfile.set_extension("png");
+    img_threaded
+        .save(&outfile)
+        .expect("Failed to save threaded image");
+}
+
+fn write_csv(
+    out_dir: &mut PathBuf,
+    prefix: &str,
+    thread_coords: &[(Array1<f64>, Array1<f64>, usize)],
+    opt: &Opt,
+) {
+    out_dir.set_file_name(format!("{}_threads", prefix));
+    out_dir.set_extension("csv");
+
+    let optional_header = opt.header.then(|| {
+        if opt.write_coords {
+            vec!["x1", "y1", "x2", "y2"]
+        } else {
+            vec!["pins"]
+        }
+    });
+
+    let mut writer = Writer::from_path(&out_dir).expect("Failed to save threads CSV");
+
+    if let Some(header) = optional_header {
+        writer
+            .write_record(&header)
+            .expect("Failed to write header");
+    }
+    let formatter = if opt.write_coords {
+        |(x_line, y_line, _): &(Array1<f64>, Array1<f64>, usize)| {
+            let last = x_line.len() - 1;
+            vec![
+                format!("{}", x_line[0] as u32),
+                format!("{}", y_line[0] as u32),
+                format!("{}", x_line[last] as u32),
+                format!("{}", y_line[last] as u32),
+            ]
+        }
+    } else {
+        |(_, _, pin): &(Array1<f64>, Array1<f64>, usize)| vec![format!("{}", pin)]
+    };
+    for thread_coord in thread_coords {
+        writer
+            .write_record(formatter(thread_coord))
+            .expect("Failed to write thread");
+    }
+}
+#[cfg(test)]
+mod tests {
+    use clap::IntoApp;
+
+    use super::*;
+
+    #[test]
+    fn verify_app() {
+        Opt::into_app().debug_assert();
+    }
 }
